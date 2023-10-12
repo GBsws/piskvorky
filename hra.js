192fc190410dcb5ad216eabe9053070ddd0f4eca
Gabriela Biswasová <pracho.dayat@seznam.cz>
2023-10-12 18:24:31 +0200

    oprava barvy, pole

--- hra.js
+++ hra.js
@@ -7,20 +7,39 @@ const tah = (event) =>{
     if (currentPlayer === 'circle'){
         event.target.classList.toggle('hra__button--circle')
         currentPlayer = 'cross'
-        whoPlays.src = 'cross_black.svg'
+        whoPlays.src = 'cross.svg'
     } else{
         event.target.classList.toggle('hra__button--cross')
         currentPlayer ='circle'
-        whoPlays.src = 'circle_black.svg'
+        whoPlays.src = 'circle.svg'
     }
-event.target.disabled = true
-
-
+    event.target.disabled = true
+    
 }
+let btnArray = []
 const tahElm = document.querySelectorAll('.hra__button')
-tahElm.forEach((button)=>{
+tahElm.forEach(button=>{
     button.addEventListener('click',tah)
 })
+tahElm.forEach(kostka=>{
+    let sign = kostka.textContent
+    if(sign===''){
+        sign='_'
+    }
+    btnArray.push(sign)
+})
+console.log(tahElm)
+
+const gameFinish = findWinner(btnArray)
+if(gameFinish.src === 'circle_black.svg' || gameFinish.src === 'cross_black.svg'){
+    console.log('Vyhrál', gameFinish)
+} else if (gameFinish==='tie'){
+    console.log ('remiza')
+} else {
+    console.log('Zatim nikdo nevyhrál')
+}
+
+
 
 //--------------------------------------------------------------------
 
